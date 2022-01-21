ef66a91322316d308345460dd21e7e5f3dcaf5ac
yellow-flamingo <amymason1211@gmail.com>
2022-01-21 11:53:46 +0000

    added different screens

---
+++ core/src/dev/thelabradors/yorkpirates/TitleScreen.java
@@ -0,0 +1,44 @@
+package dev.thelabradors.yorkpirates;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+
+public class TitleScreen extends ScreenAdapter {
+
+    YorkPiratesGame game;
+
+    public TitleScreen(YorkPiratesGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            @Override
+            public boolean keyDown(int keyCode) {
+                if (keyCode == Input.Keys.SPACE) {
+                    game.setScreen(new GameScreen(game));
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, .25f, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.font.draw(game.batch, "Title Screen!", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);
+        game.font.draw(game.batch, "Pres space to play", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .25f);
+        game.batch.end();
+    }
+
+    @Override
+    public void hide() { Gdx.input.setInputProcessor(null); }
+}
+
+
--- core/src/dev/thelabradors/yorkpirates/YorkPiratesGame.java
+++ core/src/dev/thelabradors/yorkpirates/YorkPiratesGame.java
@@ -4,28 +4,40 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.Input;
 
-public class YorkPiratesGame extends ApplicationAdapter {
+public class YorkPiratesGame extends Game {
 	SpriteBatch batch;
-	Texture img;
+	ShapeRenderer shapeRenderer;
+	//Texture img;
+	BitmapFont font;
 	
 	@Override
 	public void create () {
+		shapeRenderer = new ShapeRenderer();
+		font = new BitmapFont();
+		setScreen(new TitleScreen(this));
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
 	}
 
-	@Override
+/*	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
-	}
+	}*/
 	
 	@Override
 	public void dispose () {
+		font.dispose();
 		batch.dispose();
-		img.dispose();
+		//img.dispose();
 	}
 }
