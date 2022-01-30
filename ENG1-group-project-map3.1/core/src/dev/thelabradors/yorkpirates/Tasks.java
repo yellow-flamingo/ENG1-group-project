3adbe3f7e35892626ffedd233b8871a54a85af5f
KeiraL11 <94186674+KeiraL11@users.noreply.github.com>
2022-01-30 16:24:54 +0000

    Add files via upload

---
+++ ENG1-group-project-map3.1/core/src/dev/thelabradors/yorkpirates/Tasks.java
@@ -0,0 +1,57 @@
+package dev.thelabradors.yorkpirates;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.ApplicationListener;
+
+
+
+public class Tasks extends ApplicationAdapter{
+    private SpriteBatch batch;
+    private BitmapFont font;
+    static String task;
+
+    @Override
+    public void create(){
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
+        task = getNewTask();
+    }
+
+    public static String getNewTask() {
+        if(Collect5Coins()==false){
+            task = "Collect 5 coins";
+        }
+        else{
+            
+        }
+        return task;
+    }
+
+    public static boolean Collect5Coins(){
+        //Need a counter of coins to see if 5 coins has been collected.
+        return false;
+    }
+
+    @Override
+    public void dispose(){
+        batch.dispose();
+        font.dispose();
+    }
+
+    @Override
+    public void render(){
+        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        font.setColor(Color.WHITE);
+        font.draw(batch, task, 10, 10);
+        batch.end();
+    }
+
+}
